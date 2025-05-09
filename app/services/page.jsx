54a5920ef99222122a21b9da55d2c3a5e5722e40
Leoric01 <troglodit.nick@seznam.cz>
2025-05-09 02:21:06 +0200

    started w services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services = [
+  {
+    num: "01",
+    title: "Backend development",
+    description: "Java, Springboot, Maven, Gradle, SQL",
+    href: "",
+    icon: <BsArrowDownRight />,
+  },
+  {
+    num: "02",
+    title: "Frontend development",
+    description: "React, Typescript, Angular",
+    href: "",
+    icon: <BsArrowDownRight />,
+  },
+  {
+    num: "03",
+    title: "DevOps",
+    description: "Git, Jenkins, ELK, Docker",
+    href: "",
+    icon: <BsArrowDownRight />,
+  },
+];
+
+import { motion } from "framer-motion";
 
 const Services = () => {
-  return <div>services page</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+        >
+          {services.map((service, index) => {
+            return (
+              <div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
+                <div>
+                  <div className="text-accent text-2xl font-bold">{service.num}</div>
+                  <Link href={service.href} className="text-accent flex items-center gap-2 mt-2">
+                    {service.icon} View more
+                  </Link>
+                </div>
+                <h3 className="text-xl font-semibold">{service.title}</h3>
+                <p className="text-white/80">{service.description}</p>
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
