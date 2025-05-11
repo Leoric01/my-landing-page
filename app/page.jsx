db64e3f12ef036abdbb920cd2c90caea85421883
Leoric01 <troglodit.nick@seznam.cz>
2025-05-11 21:52:36 +0200

    fixed page render to be after stair transition

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 import { useEffect, useState } from "react";
@@ -39,44 +40,52 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-        <div className="text-center xl:text-left order-2 xl:order-none">
-          <span>Software Developer</span>
-          <h1 className="h1 mb-6">
-            Hello, I'm <br /> <span className="text-accent">Jiří Urban</span>
-          </h1>
-          <p className="max-w-[500px] mb-9 text-white/80">
-            I excel at backend using java as my main, but i have also some basics in frontend with react, typescript,
-            even created one project using angular but design is my weakpoint. <br />
-            Other techstack experience: springboot, maven, gradle, git, sql, jira, confluence, swagger, hibernate,
-            junit, ELK, jenkins.
-          </p>
-          <p className="mb-4 text-white/80">
-            Current experience as an active programmer:{" "}
-            <span className="font-semibold">
-              {experience.years}y / {experience.months}m / {experience.days}d
-            </span>
-          </p>
-          <div className="flex flex-col xl:flex-row items-center gap-8">
-            <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
-              <span>Download CV</span>
-              <FiDownload className="text-xl" />
-            </Button>
-            <div className="mb-8 xl:mb-0">
-              <Social
-                containerStyles="flex gap-6"
-                iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
-              />
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span>Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello, I'm <br /> <span className="text-accent">Jiří Urban</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at backend using java as my main, but i have also some basics in frontend with react, typescript,
+              even created one project using angular but design is my weakpoint. <br />
+              Other techstack experience: springboot, maven, gradle, git, sql, jira, confluence, swagger, hibernate,
+              junit, ELK, jenkins.
+            </p>
+            <p className="mb-4 text-white/80">
+              Current experience as an active programmer:{" "}
+              <span className="font-semibold">
+                {experience.years}y / {experience.months}m / {experience.days}d
+              </span>
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
             </div>
           </div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
-        <div className="order-1 xl:order-none mb-8 xl:mb-0">
-          <Photo />
-        </div>
+        <Stats />
       </div>
-      <Stats />
-    </div>
+    </motion.section>
   );
 }
 
